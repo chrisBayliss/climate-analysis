164a3a4fb88c7aedbcd023ce0498041ac76f157c
chris bayliss <c.bayliss@soton.ac.uk>
2015-12-16 14:52:31 +0000

    unit tests added

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,5 +1,10 @@
 """A library to perform temperature conversions"""
 
+def test_non_negative_kelvin():
+	assert fahr_to_kelvin(0)>=0, "kelvin must be non-negative"
+	
+#def test_kelvin
+
 def fahr_to_celsius(fahr):
     """Convert Fahrenheit to Celsius.
 
@@ -20,4 +25,5 @@ def fahr_to_kelvin(fahr):
     fahr -- the temperature in Fahrenheit
     """
     kelvin = fahr_to_celsius(fahr) + 273.15
+	
     return kelvin
