17c6d1c0e727b3c4dfec3b030805416010cdd200
chris bayliss <c.bayliss@soton.ac.uk>
2015-12-16 15:07:08 +0000

    separate unit test python file added again point 2

--- test_temp_conversion.py
+++ test_temp_conversion.py
@@ -1,3 +1,8 @@
 from temp_conversion import fahr_to_kelvin
+from temp_conversion import fahr_to_celsius
 def test_non_negative_kelvin():
 	assert fahr_to_kelvin(0)>=0, "kelvin must be non-negative"
+	
+def test_fahro():
+	#print(fahr_to_celsius(0)+","+(160/9))
+	assert fahr_to_celsius(0)==(-160/9),"fahr 0 test"
